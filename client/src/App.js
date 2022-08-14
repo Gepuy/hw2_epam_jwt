90d785b37b6a182e4c3e4e724190dcee984b66f8
Gepuy <maxbondarev93@gmail.com>
2022-08-14 23:50:50 +0300

    feat: start build authpage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 import AppRouter from "./components/AppRouter";
-import {getUserNotes} from "./api/notesApi";
 import NavBar from "./components/NavBar";
 
-
 function App() {
-    getUserNotes().then(data => data);
     return (
         <>
             <NavBar/>
